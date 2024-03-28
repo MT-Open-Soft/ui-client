b05ff9abf41d6a235957a9ca5e43e9021c9d6f8c
Ishita Bansal <ishitabansal770@gmail.com>
2024-03-29 03:09:21 +0530

    Changes with 1 error

---
+++ src/components/Language.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
+// Initialize Swiper core components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+const Language = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={4}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Language;
