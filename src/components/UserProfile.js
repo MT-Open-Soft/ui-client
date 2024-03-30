73fedaff3a0360c8800f568e73e82ded50a45fb8
Nafisa Parvin <parvinnafisa21@gmail.com>
2024-03-30 09:15:54 +0530

    Made changes to Profile Page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,106 +1,210 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
 import { FaGem, FaStar, FaCircle } from 'react-icons/fa';
+import axios from 'axios';
+
+const SubscriptionBadge = ({ user_plan }) => {
+  let icon = null;
+  switch (user_plan?.toLowerCase() || 'default') {
+    case 'gold': 
+    icon = <FaGem color='#eab308' />
+    break;
+    case 'silver': 
+    icon = <FaStar color='#cbd5e1' />
+    break;
+    default: 
+    icon = <FaCircle color='#1d4ed8' />;
+  }
+  return (
+    <div className="flex items-center my-2 justify-center" style={{ textAlign: "center" }}>
+      {icon}
+      <span className="ml-2 pl-2">{user_plan}</span>
+    </div>
+  );
+};
 
 const ProfilePage = ({ userPassword, onPasswordChange }) => {
-  const userData = {
-    src:'/bgimg.jpg',
-    username: 'John Doe',
-    email: 'johndoe@example.com',
-    subscriptionPlan: 'Gold',
-    password:'u3wh4u2' 
+  var token = localStorage.getItem('token');
+  const apiURL = "http://localhost:8080/api/v1/user";
+  const [userData, setUserData] = useState(null);
+  const userDisplay = {
+    src: '/default-avatar.png',
+    username: userData?.name || 'Username',
+    email: userData?.email || 'Email',
+    plan: userData?.subscription || 'free',
   };
-  const SubscriptionBadge = ({ plan = userData.subscriptionPlan }) => {
-    let icon = null;
-  switch (plan.toLowerCase()) {
-    case 'gold':
-      icon = <FaGem />;
-      break;
-    case 'silver':
-      icon = <FaStar />;
-      break;
-    default:
-      icon = <FaCircle />;
-    }
-    return (
-        <div className="flex items-center my-2 justify-center"  style={{textAlign:"center"}}>
-          {icon}
-          <span className="ml-2 pl-2 mb-1">{plan}</span>
-        </div>
-      );
-    };
+  useEffect(() => {
+    const getUserData = async () => {
+      const result = await axios
+      .get(`${apiURL}`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch((e) => {
+        console.error(e)
+        setError(e.response.data.error.message);
+      });
+      console.log('result', result)
+    if(result) {
+      setUserData(result);
+    }}
+    getUserData();
+    
+  }, []);
+  
+  
+  console.log("userData", userData);
+  
+  console.log("userDisplay", userDisplay);
   const [isEditing, setIsEditing] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const [password, setPassword] = useState(userData.password);
+  const [showOldPassword, setOldShowPassword] = useState(false);
+  const [showNewPassword, setNewShowPassword] = useState(false);
+  const [showConfirmNewPassword, setConfirmNewShowPassword] = useState(false);
+  const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
   const [passwordMatch, setPasswordMatch] = useState(true);
+  const [updatePasswordError, setUpdatePasswordError] = useState('');
+  const [emptyFieldError, setEmptyFieldError] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [deleteError, setDeleteError] = useState(false);
+  const [deleteSuccess, setDeleteSuccess] = useState(false);
 
-  const handleTogglePassword = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
-  };
-  
-  const handleChangePassword = (e) => {
-    e.preventDefault(); 
-    setPassword(e.target.value);
-    setNewPassword('');
-    setConfirmNewPassword('');
-  };
+  const handleToggleOldPassword = () => {
+        setOldShowPassword((prevOldShowPassword) => !prevOldShowPassword);
+      };
+      const handleToggleNewPassword = () => {
+        setNewShowPassword((prevNewShowPassword) => !prevNewShowPassword);
+      };
+      const handleToggleConfirmNewPassword = () => {
+        setConfirmNewShowPassword((prevConfirmNewShowPassword) => !prevConfirmNewShowPassword);
+      };
+    
+      const handleOldPasswordChange = (e) => {
+        setOldPassword(e.target.value);
+        isEmptyField();        
+      };
 
-  const handleNewPasswordChange = (e) => {
-    setNewPassword(e.target.value);
-    if (confirmNewPassword !== '' && e.target.value !== confirmNewPassword) {
-      setPasswordMatch(false);
-    } else {
-      setPasswordMatch(true);
+      const handleNewPasswordChange = (e) => {
+        setNewPassword(e.target.value);
+        if (confirmNewPassword !== '' && e.target.value !== confirmNewPassword) {
+          setPasswordMatch(false);
+        } else {
+          setPasswordMatch(true);
+        }
+        isEmptyField();
+      };
+    
+      const handleConfirmPasswordChange = (e) => {
+        setConfirmNewPassword(e.target.value);
+        if (e.target.value !== newPassword) {
+          setPasswordMatch(false);
+        } else {
+          setPasswordMatch(true);
+        }
+        isEmptyField();
+      };
+
+      const isEmptyField = () => {
+        if(oldPassword === '' || newPassword === '' || confirmNewPassword === '') {
+          setEmptyFieldError(true);
+        } else {
+          setEmptyFieldError(false);
+        }
+      }
+    
+      const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(!oldPassword || !newPassword || !confirmNewPassword) {
+          setEmptyFieldError(true);
+          console.log('Please fill in all fields');
+          return;
+        }
+        console.log("token",token)
+    
+        const result = await axios
+            .put(`${apiURL}/password`, {oldPassword, newPassword}, {
+              headers: {
+                "Authorization": `Bearer ${token}`
+              }
+            })
+            .catch((e) => {
+              console.error(e)
+              setIsError(true);
+              setUpdatePasswordError(e.response.data.error.message);
+            });
+    
+        if(result) {
+          setEmptyFieldError(false)
+          setIsError(false)
+          setDeleteError(false);
+          setSuccess(true);
+          setOldPassword('');
+          setNewPassword('');
+          setConfirmNewPassword('');
+          setIsEditing(false);
+        }    
     }
-  };
-
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmNewPassword(e.target.value);
-    if (e.target.value !== newPassword) {
-      setPasswordMatch(false);
-    } else {
-      setPasswordMatch(true);
-    }
-  };
-
+    
+    const handleDeleteAccount = async(e) => {
+      e.preventDefault();
+      const result = await axios
+          .delete(`${apiURL}`, {
+            headers: {
+              "Authorization": `Bearer ${token}`
+            }
+          })
+          .catch((e) => {
+            console.error(e)
+            setDeleteError(true);
+            setError(e.response.data.error.message);
+          });
+          
+        if(result) {
+          setDeleteSuccess(false);
+          localStorage.removeItem('token');
+          window.location.href = '/';
+        }
+      }
 
   return (
-    <div className="relative flex flex-col items-center min-h-screen bg-[#152238]" style= {{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          backgroundImage:  "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(bg.webp)", 
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          zIndex: -1
-          }}>
-      <div className="bg-[#091121] p-5 rounded-lg shadow-md mb-5 mt-16 w-[500px] h-auto z-50">
+    <div className="relative flex flex-col items-center min-h-screen bg-[#152238]" style={{
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+      backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(bg.webp)",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      zIndex: 1
+    }}>
+      <div className="bg-[#091121] p-5 rounded-lg shadow-md mb-5 mt-16 w-[500px] h-auto z-50 box-border border-2 border-yellow-600">
         <img
-          src={userData.src}
+          src={userDisplay.src}
           alt="Profile"
           className="rounded-full h-24 w-24 object-cover justify-center mx-auto"
         />
         <div className='bg-[#293b5e] rounded-lg shadow-md my-6 py-4 px-4 text-white text-semibold h-[250px]'>
-            <div className='py-2 my-2 scale-150' style={{textTransform: 'uppercase', textAlign: 'center',letterSpacing: '0.3em'}}>
-            {SubscriptionBadge(userData.subscriptionPlan)}
-
-            </div>
-            <div className='flex py-3 my-2 scale-125 justify-center'><FaEnvelope className='inline-flex mr-4 mt-1'  /> {userData.email}</div>
-            <div className='flex py-1 my-2 scale-125 justify-center'><FaUser className='inline-flex mr-4 mt-1' />
-            {userData.username}
-
-            </div>
+          <div className='py-2 my-2 text-2xl' style={{ textTransform: 'uppercase', textAlign: 'center', letterSpacing: '0.3em' }}>
+            <SubscriptionBadge user_plan={userDisplay.plan} />
+          </div>
+          <div className='flex py-3 my-2 text-xl justify-start'><FaEnvelope className='inline-flex mr-4 mt-1' /> {userDisplay.email}</div>
+          <div className='flex py-1 my-2 text-xl justify-start'><FaUser className='inline-flex mr-4 mt-1' />{userDisplay.username}</div>
         </div>
-        <form>
-          
+
+        <form name='password_change' onSubmit>
           <div>
             {isEditing ? (<></>) 
             : (
-                <>
+              <div>
                 <div className='flex flex-row mx-auto justify-around'>
                   <div>
                     <button
@@ -108,55 +212,56 @@ const ProfilePage = ({ userPassword, onPasswordChange }) => {
                       onClick={() => setIsEditing(true)}
                       className="bg-[#152238] text-white px-4 py-2 rounded mt-4 mx-5"
                     >
-                      Edit Password
+                    Edit Password
                     </button>
                   </div>
                   <div>
                     <button
                       type="button"
-                      onClick={() => setIsEditing(true)}
+                      onClick={handleDeleteAccount}
                       className="bg-red-500 text-white px-4 py-2 rounded mt-4 mx-5"
                     >
-                      Delete Account
+                    Delete Account
                     </button>
                   </div>
                 </div> 
-                </>
-
+                {success && <div className='text-green-500 text-xs italic text-center my-4'>Password changed successfully</div>}
+                  {deleteSuccess && <div className='text-green-500 text-xs italic text-center my-4'>Account deleted successfully</div>}
+              </div>
             )}
           </div>
           {isEditing && (
             <div className="flex flex-col items-center my-2 justify-items-center">
-              <label htmlFor="password" className="mr-2 text-white py-2">
-                Password
+              <label className="mr-2 text-lg text-white py-2">
+                Edit Password
               </label>
               <div className='flex'>
                 <input
                   id="old_password"
-                  type={showPassword ? 'text' : 'password'}
+                  value={oldPassword}
+                  type={showOldPassword ? 'text' : 'password'}
                   className="px-2 py-2 mb-2 w-md bg-[#293b5e] text-white rounded-lg"
-                  placeholder="Enter old password"
+                  placeholder="Old password"
                   required
-                  alert="Please enter your old password"
+                  onChange={handleOldPasswordChange}
                 />
                 <button
                     type="button"
                     onClick={() => {
-                      handleTogglePassword();
-                      setIsEditing(true);
+                      handleToggleOldPassword();
                     }}
                     className="text-blue-500 focus:outline-none ml-2"
                   >
-                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    {showOldPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
               <div className='flex'>
                 <input
                   id="new_password"
                   value={newPassword}
-                  type={showPassword ? 'text' : 'password'}
+                  type={showNewPassword ? 'text' : 'password'}
                   className="px-2 py-2 mb-2 w-md bg-[#293b5e] text-white rounded-lg"
-                  placeholder="Enter new password"
+                  placeholder="New password"
                   required
                   alert="Please enter your new password"
                   onChange={handleNewPasswordChange}
@@ -164,19 +269,18 @@ const ProfilePage = ({ userPassword, onPasswordChange }) => {
                 <button
                     type="button"
                     onClick={() => {
-                      handleTogglePassword();
-                      setIsEditing(true);
+                      handleToggleNewPassword();
                     }}
                     className="text-blue-500 focus:outline-none ml-2"
                   >
-                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    {showNewPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
               <div className='flex'>
                 <input
                   id="confirm_new_password"
                   value={confirmNewPassword}
-                  type={showPassword ? 'text' : 'password'}
+                  type={showConfirmNewPassword ? 'text' : 'password'}
                   className="px-2 py-2 mb-2 w-md bg-[#293b5e] text-white rounded-lg"
                   placeholder="Confirm new password"
                   required
@@ -187,16 +291,19 @@ const ProfilePage = ({ userPassword, onPasswordChange }) => {
                 <button
                     type="button"
                     onClick={() => {
-                      handleTogglePassword();
-                      setIsEditing(true);
+                      handleToggleConfirmNewPassword();
                     }}
                     className="text-blue-500 focus:outline-none ml-2"
                   >
-                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    {showConfirmNewPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
-              {!passwordMatch && <p className='text-red-500'>Passwords do not match</p>}
-              <button id='submit' disabled={!passwordMatch && true} onClick={handleChangePassword} className="bg-gray-800 text-slate-200 hover:bg-[#152238] hover:text-white px-4 py-2 rounded mt-4">
+              {!passwordMatch && <div className='text-red-500 text-xs italic'>Passwords do not match</div>}
+              {emptyFieldError && <div className='text-red-500 text-xs italic'>Please fill in all fields</div>}
+              {isError && <div className='text-red-500 text-xs italic'>{error}</div>}
+              {isError && <div className='text-red-500 text-xs italic'>{updatePasswordError}</div>}
+              {deleteError && <div className='text-red-500 text-xs italic'>{error}</div>}
+              <button id='submit' disabled={!passwordMatch && true} onClick={handleSubmit} className="bg-gray-800 text-slate-200 hover:bg-[#152238] hover:text-white px-4 py-2 rounded mt-4">
                 Save Changes
               </button>
             </div>
@@ -204,7 +311,7 @@ const ProfilePage = ({ userPassword, onPasswordChange }) => {
           
         </form>
       </div>
-      </div>
+    </div>
   );
 };
 
